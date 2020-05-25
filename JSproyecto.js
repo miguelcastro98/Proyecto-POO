30fb71800841729b89ae4c69eb2b064f0342285e
Miguel Castro <60783141+miguelcastro98@users.noreply.github.com>
2020-05-25 00:10:55 -0500

    Add files via upload
    Proyecto Final de Programación Orientada a Objetos
    Proyecto elaborado por Miguel Castro.

---
+++ JSproyecto.js
@@ -0,0 +1,71 @@
+﻿//Este es el apartado donde se realizan las operaciones de los numeros que se ingresan en los recuadros//
+//uso el function para invocar mis operaciones
+//el parseInt lo uso para que me devuelva un numerl, en este caso me devuelve un numero despues de haber echo una operacion
+function operaciones(op) {
+
+    var ops = {
+        sumar: function sumarNumeros(n1, n2) {
+            return (parseInt(n1) + parseInt(n2));
+        },
+
+        restar: function restarNumeros(n1, n2) {
+            return (parseInt(n1) - parseInt(n2));
+        },
+
+        multiplicar: function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));
+        },
+
+        dividir: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+
+    };
+
+    //declaro mis variables num1 y num2 que son los numeros que se ingresaran para realizar mis operaciones
+
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+
+    //Se revisa que si hay numeros dentro de los cuadros//
+    //el if y el else if los uso para declarar que si se ingresa un numro dentro de mi variable num1 y num2 y estos son verdaderos se ejecuta las condiciones
+
+    if (isNaN(parseFloat(document.getElementById('num1').value))) {
+        document.getElementById('resultado').innerHTML = "<span style='color: red;'>Por favor, ingrese un número en la primera casilla.</span>";
+        document.getElementById("num1").innerText = "0";
+        document.getElementById("num1").focus();
+    } else if (isNaN(parseFloat(document.getElementById('num2').value))) {
+        document.getElementById('resultado').innerHTML = "<span style='color: red;'>Por favor, ingrese un número en la segunda casilla.</span>";
+        document.getElementById("num2").innerText = "0";
+        document.getElementById("num2").focus();
+    }
+
+        //Cuando los numeros son ingresados correctamente se hace la operacion//
+
+    else {
+        //El switch sirve para poder mandar los resultados de las operaciones que ya se hicieron
+
+        switch (op) {
+            case 'sumar':
+                var resultado = ops.sumar(num1, num2);
+                document.getElementById('resultado').innerHTML = "<span>" + resultado + "</span>";
+                break;
+            case 'restar':
+                var resultado = ops.restar(num1, num2);
+                document.getElementById('resultado').innerHTML = "<span>" + resultado + "</span>";
+                break;
+            case 'multiplicar':
+                var resultado = ops.multiplicar(num1, num2);
+                document.getElementById('resultado').innerHTML = "<span>" + resultado + "</span>";
+                break;
+            case 'dividir':
+                var resultado = ops.dividir(num1, num2);
+                document.getElementById('resultado').innerHTML = "<span>" + resultado + "</span>";
+                break;
+
+        }
+    }
+
+}
---
+++ Stock.js
@@ -0,0 +1,22 @@
+﻿//array, en donde declaro que materiales tengo dentro de mi stock//
+
+var enStock = ['Cemento', 'Polvo', 'Grava', 'Gravilla', 'Alambre recocida', 'Varilla', 'Cal', 'Clavos', 'Armex'];
+//Se imprime mi stock//
+
+function print(message) {
+    document.write('<p>' + message + '</p>');
+}
+
+//Este es el loop
+//el prompt lo utilice para que pueda haber un dialogo entre el usuario y el codigo, lo utilizo para que el usuario pueda ver que ncesita usar cierta palabra para que es stock sea visible
+do {
+    var search = prompt("Esribe 'lista' para ver la lista los productos disponibles en stock");
+    if (enStock.indexOf(search) >= 0) {
+        //el toLowerCase se usa para poner las palabras en minisculas//
+        //else if lo uso para que cuando se escriba la palabra Lista este vea que es verdadero y la operacion termina en ese punto 
+    } else if (search.toLowerCase() === 'lista') {
+        print(enStock.join(', '));
+        //el break lo uso para que cuando se escriba la palabra "lista" este deje de buscar y la operación se de por terminada y me pueda aparecer mi stock
+        break;
+    }
+} while (search.toLowerCase() !== '')
